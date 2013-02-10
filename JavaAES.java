90d6a5499e7ccc3b094b58c5ee12cac076a600f0
brian wagner <brian@brianthetall.com>
2013-02-09 21:16:13 -0700

    working out key bugs in JavaAES

--- JavaAES.java
+++ JavaAES.java
@@ -1,23 +1,40 @@
 import java.security.*;
-import javax.crypto.Cipher;
+import javax.crypto.*;
 
 public class JavaAES{
-    private byte[] input,output;
-    private KeyGenerator key = KeyGenerator.getInstance("AES");
-    private transient Key generatedKey;
+
+    private byte[] output;
+    private KeyGenerator key;
+    private /*transient*/ Key generatedKey;
     private Cipher cipher;
-    public JavaAES(){
+
+    public JavaAES() throws Exception{
+	key = KeyGenerator.getInstance("AES");
 	key.init(128);
 	generatedKey = key.generateKey();
-	cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	cipher.init(Cipher.ENCRYPTMODE,generatedKey);
-	output = cipher.doFinal(input);
-	System.out.println(new String(output));
+	System.out.println("Generated AES128 Key = "+generatedKey.getEncoded());
+	cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");//AES/CBC/PKCS5Padding
     }
-    public encrypt(String plain){
-	
-    }
-    public decrypt(String cipher){
 
+    public byte[] encrypt(String plainText) throws Exception{
+	cipher.init(Cipher.ENCRYPT_MODE,generatedKey);
+	output = cipher.doFinal(plainText.getBytes());
+	System.out.println("Cipher Text: " + new String(output));
+	return output;
     }
-|
+
+    public byte[] decrypt(String cipherText) throws Exception{
+	cipher.init(Cipher.DECRYPT_MODE,generatedKey);
+	output = cipher.doFinal(cipherText.getBytes());
+	System.out.println("Plain Text: " + new String(output));
+	return output;
+	//zero key[]
+    }
+    
+    public static void main(String args[]) throws Exception{
+	System.out.println("# ARGS = "+args.length);
+	JavaAES crypt = new JavaAES();
+	crypt.encrypt(args[0]);
+	crypt.decrypt(args[0]);
+    }
+}
