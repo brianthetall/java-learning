274e93f5463964c31a1baa412b86dcfb9fa872d4
brian wagner <brian@brianthetall.com>
2013-02-11 13:51:50 -0700

    cleaned JavaAES, finished.

--- JavaAES.java
+++ JavaAES.java
@@ -7,29 +7,25 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 
 public class JavaAES{
-
+    private static final int DEBUG=0;
     private byte[] output, IV;
     private KeyGenerator keyGen;
     private transient Key key;
     private Cipher cipher;
-    private AlgorithmParameters parameters;
 
     public JavaAES() throws Exception{
-	parameters = AlgorithmParameters.getInstance("AES");
-
 	keyGen = KeyGenerator.getInstance("AES");
 	keyGen.init(128);
 	key = keyGen.generateKey();
-	System.out.println("Generated AES Key = "+new String(key.getEncoded(),"UTF8") + "\r\nKey Algorithm="+key.getAlgorithm());
+	if(DEBUG==1){System.out.println("Generated AES Key = "+new String(key.getEncoded(),"UTF8") + "\r\nKey Algorithm="+key.getAlgorithm());}
 	cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	//	cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
     }
 
     public byte[] encrypt(String plainText) throws Exception{
 	cipher.init(Cipher.ENCRYPT_MODE,key);
 	output = cipher.doFinal(plainText.getBytes());
-	System.out.println("Cipher Text: " + new String(output,"UTF8"));
-	System.out.println("Cipher IV: "+new String(cipher.getIV(),"UTF8"));
+	if(DEBUG==1){System.out.println("Cipher Text: " + new String(output,"UTF8"));}
+	if(DEBUG==1){System.out.println("Cipher IV: "+new String(cipher.getIV(),"UTF8"));}
 	IV = cipher.getIV();
 	return output;
     }
@@ -37,15 +33,16 @@ public class JavaAES{
     public byte[] decrypt(byte[] cipherText) throws Exception{
 	cipher.init(Cipher.DECRYPT_MODE,key,new IvParameterSpec(IV));
 	output = cipher.doFinal(cipherText);
-  	System.out.println("Plain Text: " + new String(output));
+	if(DEBUG==1){System.out.println("Plain Text: " + new String(output));}
 	return output;
 	//zero key[]
     }
     
     public static void main(String args[]) throws Exception{
-	System.out.println("# ARGS = "+args.length);
+	if(args.length!=1)
+	    System.exit(-1);
 	JavaAES crypt = new JavaAES();
-	;
-	crypt.decrypt(crypt.encrypt(args[0]));
+	if(args[0].equals(new String(crypt.decrypt(crypt.encrypt(args[0])))))
+	    System.out.println("Class: JavaAES is working!");
     }
 }
