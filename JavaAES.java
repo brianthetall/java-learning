72bb674304e0f6e098a43b71c11677892d425c54
brian wagner <brian@brianthetall.com>
2013-02-11 11:28:54 -0700

    JavaAES using ECB is working; next figure out the IV issue and switch to CBC

--- JavaAES.java
+++ JavaAES.java
@@ -1,32 +1,35 @@
-import java.security.*;
-import javax.crypto.*;
+import java.security.SecureRandom;
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 
 public class JavaAES{
 
     private byte[] output;
-    private KeyGenerator key;
-    private /*transient*/ Key generatedKey;
+    private KeyGenerator keyGen;
+    private /*transient*/ Key key;
     private Cipher cipher;
 
     public JavaAES() throws Exception{
-	key = KeyGenerator.getInstance("AES");
-	key.init(128);
-	generatedKey = key.generateKey();
-	System.out.println("Generated AES128 Key = "+generatedKey.getEncoded());
-	cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");//AES/CBC/PKCS5Padding
+	keyGen = KeyGenerator.getInstance("AES");
+	keyGen.init(128);
+	key = keyGen.generateKey();
+	System.out.println("Generated AES Key = "+new String(key.getEncoded(),"UTF8") + "\r\nKey Algorithm="+key.getAlgorithm());
+	//	cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
     }
 
     public byte[] encrypt(String plainText) throws Exception{
-	cipher.init(Cipher.ENCRYPT_MODE,generatedKey);
+	cipher.init(Cipher.ENCRYPT_MODE,key);
 	output = cipher.doFinal(plainText.getBytes());
-	System.out.println("Cipher Text: " + new String(output));
+	System.out.println("Cipher Text: " + new String(output,"UTF8"));
 	return output;
     }
 
-    public byte[] decrypt(String cipherText) throws Exception{
-	cipher.init(Cipher.DECRYPT_MODE,generatedKey);
-	output = cipher.doFinal(cipherText.getBytes());
-	System.out.println("Plain Text: " + new String(output));
+    public byte[] decrypt(byte[] cipherText) throws Exception{
+	cipher.init(Cipher.DECRYPT_MODE,key);
+	output = cipher.doFinal(cipherText);
+  	System.out.println("Plain Text: " + new String(output));
 	return output;
 	//zero key[]
     }
@@ -34,7 +37,7 @@ public class JavaAES{
     public static void main(String args[]) throws Exception{
 	System.out.println("# ARGS = "+args.length);
 	JavaAES crypt = new JavaAES();
-	crypt.encrypt(args[0]);
-	crypt.decrypt(args[0]);
+	;
+	crypt.decrypt(crypt.encrypt(args[0]));
     }
 }
