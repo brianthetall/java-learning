b24cff0432faae6268f4e3afb667490fa97a1dec
Brian Wagner <brianthetall@gmail.com>
2013-07-08 21:15:46 -0600

    Heap can add,poll,peek,sort

--- Sorting/Heap.java
+++ Sorting/Heap.java
@@ -1,5 +1,3 @@
-//import java.lang.ClassCastException;
-
 public class Heap<T extends Comparable>{
 
     public static class Node<T extends Comparable> implements Comparable{
@@ -48,12 +46,19 @@ public class Heap<T extends Comparable>{
 	
     }
 
+    private int findChild(int index){
+
+	if(index<0)
+	    return -1;
+	
+	return index * base + 1;
+    }
+
     private void heapup(int index){
 
 	int p=findParent(index);
 	if( p >= 0 && data[index].compareTo(data[p]) < 0 ){
 
-	    System.out.println("SWAP index="+index+"with parent@:"+p);	    
 	    Node<T> temp = data[p];
 	    data[p]=data[index];
 	    data[index]=temp;
@@ -63,6 +68,34 @@ public class Heap<T extends Comparable>{
 
     }
 
+    private void heapdown(int index){
+	
+	int firstChild=findChild(index);
+	if(data[index]!=null){
+	    T largestChild=(T)data[index].getValue();//init to parent's value
+	    int largestIndex=-1;
+	    for(int i=firstChild ; i<firstChild+base ; i++){
+		
+		if(data[i] == null)
+		    break;
+		
+		if(data[i].getValue().compareTo(largestChild) > 0){
+		    largestChild=(T)data[i].getValue();
+		    largestIndex=i;
+		}
+	    }
+	    
+	    //if any children are larger, the bigest is now in largestChild/Index   
+	    if(largestChild.compareTo(data[index].getValue()) > 0){
+		
+		Node<T> temp=data[largestIndex];
+		data[largestIndex] = data[index];
+		data[index] = temp;
+		heapdown(largestIndex);//recurse
+	    }
+	}
+    }
+
     public void add(T t){
 	data[cursor] = new Node<T>(t);
 	heapup(cursor);
@@ -70,22 +103,51 @@ public class Heap<T extends Comparable>{
     }
 
     public T poll(){
-	return null;
+
+	if(data[0]==null)
+	    return null;
+
+	Node<T> retval=data[0];
+	data[0] = data[--cursor];
+	data[cursor]=null;
+	heapdown(0);
+
+	return retval.getValue();
     }
 
     public T peek(){
-	return null;
+	return (T)data[0].getValue();
     }
 
-    public T[] sort(){
-	return null;
+    public Object[] sort(){
+	
+	Object[] retval=new Object[cursor];
+	for(int i=0;i<retval.length;i++)
+	    retval[i]=poll();
+
+	return retval;
     }
 
     public String toString(){
 	String r="";
-	for(Node n:data)
+	int i=0;
+	for(Node n:data){
+
+	    if(n==null)
+		break;
+
+	    int p2=(int)(Math.log(i+1)/Math.log(base));
+	    Double d=new Double(Math.log(i+1)/Math.log(base));
+
+	    //	    System.out.println("p2="+p2+" d="+d);
+	    if( d-p2==0 )
+		r=r.concat("|");
+
 	    if(n!=null)
 		r=r.concat(n.toString()+" ");
+	    
+	    i++;
+	}
 	return r;
     }
 
@@ -101,13 +163,41 @@ public class Heap<T extends Comparable>{
 	    data[i]=new Double(args[i]);
 
 	int base=2;
-	Heap<Double> heap=new Heap<>(data.length,base);
+	Heap<Double> heap=new Heap<>(data.length*10,base);
 
-	for(Double d:data){
+	for(Double d:data)
 	    heap.add(d);
-	    System.out.println(heap);
-	}
-	    	
+	System.out.println(heap);
+
+	Double peek = (Double)heap.peek();
+	Double peek1 = (Double)heap.peek();
+	System.out.println(peek +"=="+peek1);
+
+	Double pulled = (Double)heap.poll();
+	System.out.println("Polled:"+pulled);
+	System.out.println(heap);
+
+	for(Double d:data)
+	    heap.add(d);
+	System.out.println(heap);
+
+	pulled = (Double)heap.poll();
+	System.out.println("pulled="+pulled);
+	pulled = (Double)heap.poll();
+	System.out.println("pulled="+pulled);
+	pulled = (Double)heap.poll();
+	System.out.println("pulled="+pulled);
+	System.out.println(heap);
+
+	Object[] sorted = heap.sort();
+	for(Object o:sorted)
+	    System.out.println(o);
+	//	Double[] dsorted=new Double[sorted.length];
+	//	for(int i=0;i<dsorted.length;i++)
+	//	    dsorted[i] = (Double)sorted[i];
+	//	for(Double d:dsorted)
+	//	    System.out.print(d+", ");
+
     }
 
 }
