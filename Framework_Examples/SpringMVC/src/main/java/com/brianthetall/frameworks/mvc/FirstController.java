d4812e4474c8e7c30acda7fe8235e56b4ea23ca6
Brian Wagner <brianthetall@gmail.com>
2013-08-08 13:20:16 -0600

    Spring MVC: putting parts together and setting up directory structure; compiles.

--- Framework_Examples/SpringMVC/src/main/java/com/brianthetall/frameworks/mvc/FirstController.java
+++ Framework_Examples/SpringMVC/src/main/java/com/brianthetall/frameworks/mvc/FirstController.java
@@ -1,6 +1,30 @@
 package com.brianthetall.frameworks.mvc;
 
+import javax.validation.Valid;//how does this work? the javadoc was sparse.
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class FirstController{
 
+    //WHAT CALLS THIS? DISPATCHER_SERVLET???
+    @RequestMapping(value="/", method=RequestMethod.GET)
+	public String loadFormPage(Model m) {//WHERE DOES m COME FROM? DISPATCHER_SERVLET?
+	//	m.addAttribute("subscriber", new Subscriber());//does this effect the returned View?
+	return "view0";
+    }
+
+    //WHAT DOES @VALID DO?
+    //what is BindingResult? Model?
+    @RequestMapping(value="/", method=RequestMethod.POST)
+	public String submitForm(/*@Valid Subscriber subscriber, */BindingResult result, Model m) {
+	if(result.hasErrors()) {
+	    return "view0";
+	}
+	return "view0";
+    }
 }
+
---
+++ Framework_Examples/SpringMVC/src/main/webapp/WEB-INF/view/view0.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>initial view</title>
+</head>
+<body>
+<h1>
+<p>
+SpringMVC View[0]
+</p></body>
+</html>
