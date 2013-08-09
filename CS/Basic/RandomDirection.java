e21e081282dce3291c2346e977be05bf94765d72
Brian Wagner <brianthetall@gmail.com>
2013-08-08 19:52:13 -0600

    Random Direction has random delay; user specifies min and max delays in ms when calling program.

--- CS/Basic/RandomDirection.java
+++ CS/Basic/RandomDirection.java
@@ -6,11 +6,19 @@ public class RandomDirection{
 
     private SecureRandom r;
     private static final String[] directions={"LEFT","RIGHT","UP","DOWN"};
+    private static int MIN_DELAY;
+    private static int MAX_DELAY;
 
-    public RandomDirection(){
+    public RandomDirection(int minMil,int maxMil){
 	r=new SecureRandom();
+	MAX_DELAY=maxMil;
+	MIN_DELAY=minMil;
     }
 
+    /**
+     * Return a direction
+     * @return a randomly selectly direction
+     */
     public String getDirection(){
 	r.nextInt();
 	r.nextInt();
@@ -20,15 +28,32 @@ public class RandomDirection{
 	return directions[i%4];
     }
 
-    public static void main(String argv[]){
-	RandomDirection rand = new RandomDirection();
+    /**
+     * Delay roughly between min & max ms
+     */
+    public int delay()throws Exception{
+	int delayM=r.nextInt()%MAX_DELAY;
+	while(delayM<MIN_DELAY)
+	    delayM=r.nextInt()%MAX_DELAY;
+	Thread.sleep(delayM);
+	return delayM;
+    }
+
+    public static void main(String argv[])throws Exception{
+
+	RandomDirection rand=null;
+	if(argv.length!=2){
+	    System.err.println("java RandomDirection <Min Delay (ms)> <Max Delay (ms)>");
+	    System.exit(1);
+	}
+	else if(argv[0].equals(argv[1]))
+	    rand = new RandomDirection(new Integer(argv[0]).intValue(),new Integer(argv[1]).intValue()+1);
+	else
+	    rand = new RandomDirection(new Integer(argv[0]).intValue(),new Integer(argv[1]).intValue());
+	    	
 	while(true){
 	    System.out.println(rand.getDirection());
-	    try{
-		Thread.sleep(1000);
-	    }catch(Exception e){
-		System.err.println(e.getMessage());
-	    }
+	    rand.delay();
 	}
     }
 }
